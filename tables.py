eead6a2b6f60b8e0ed69b028b1be28bfc9855f5d
pixelpromohit <ramchandanimohit771@gmail.com>
2023-11-13 20:52:42 +0530

    Add run table

--- tables.py
+++ tables.py
@@ -27,6 +27,19 @@ def create_route_table():
 
     cursor.execute(query)
 
+
+def create_run_table():
+    query = f"""CREATE TABLE IF NOT EXISTS {RUN_TABLE}(
+                bus_id INTEGER,
+                running_date TEXT NOT NULL,
+                available_seats TEXT NOT NULL,
+                PRIMARY KEY(bus_id,running_date))
+                """
+    
+    print(query)
+    cursor.execute(query)
+
 if __name__ == '__main__':
     create_operator_table()
     create_route_table()
+    create_run_table()
