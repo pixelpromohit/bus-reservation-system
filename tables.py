6db279b1e2c1fe678c12ea6489d70907fae10852
pixelpromohit <ramchandanimohit771@gmail.com>
2023-11-14 14:09:14 +0530

    Add operator_table insert and edit operation

--- tables.py
+++ tables.py
@@ -23,6 +23,44 @@ def create_operator_table():
 
     cursor.execute(query)
 
+
+def add_into_operator_table(operator_id , name , address , phone , email):
+    # check if operator_id already exists
+    # exists_query  = f"""SELECT *FROM {OPERATOR_TABLE} WHERE operator_id=?"""
+    # exists = cursor.execute(exists_query, [(operator_id)])
+    # if cursor.fetchone() != None:
+    #     print(f"{operator_id} already exists in the table")
+    #     return False
+
+    try:
+        query = f"""INSERT INTO {OPERATOR_TABLE}(operator_id , name , address , phone , email)
+                    VALUES(? , ? , ? , ? , ?)
+                    """
+        cursor.execute(query , (operator_id , name , address , phone , email))
+        conn.commit()
+        return True
+    except Exception as e:
+        print(f"An error occurred= {e}")
+        return False
+
+
+def edit_into_operator_table(operator_id , name , address , phone , email):
+    exists_query  = f"""SELECT *FROM {OPERATOR_TABLE} WHERE operator_id=?"""
+    exists = cursor.execute(exists_query, [(operator_id)])
+    if cursor.fetchone() is None:
+        print(f"operator_id = {operator_id} does not exists in the table")
+        return False
+
+    try:
+        query  = f""" UPDATE {OPERATOR_TABLE} SET name = ? , address = ? , phone = ? , email = ? WHERE operator_id = ?"""
+        cursor.execute(query , (name , address , phone , email , operator_id))
+        conn.commit()
+        return True
+    except Exception as e:
+        print(f"An error occured while editing = {e}")
+        return False
+
+
 def create_route_table():
     query = f"""CREATE TABLE IF NOT EXISTS {ROUTE_TABLE}(
                 route_id INTEGER PRIMARY KEY,
@@ -71,3 +109,5 @@ def create_all_tables():
 
 if __name__ == '__main__':
    create_all_tables()
+   success = add_into_operator_table(5 , 'rohit' , 'pune' , '8087900440' , 'teamrohit@gmail.com')
+   edit_into_operator_table(88 , 'mohit ramchandani' , 'mumbai' , '8087900447' , 'gfg@gmail.com')
